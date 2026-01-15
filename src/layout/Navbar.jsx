795991e47e61ccc894dd8790dd3252ef051e4d5d
Jmena1120 <enzioac2015@gmail.com>
2026-01-15 00:04:49 -0500

    Add files via upload

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,24 +1,21 @@
-import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../AuthContext";
 
-/** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
-  const { setPage } = usePage();
+
   return (
-    <header>
-      <p>Fitness Trackr</p>
-      <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
-        {token ? (
-          <a onClick={() => logout()}>Log out</a>
-        ) : (
-          <>
-            <a onClick={() => setPage("register")}>Register</a>
-            <a onClick={() => setPage("login")}>Login</a>
-          </>
-        )}
-      </nav>
-    </header>
+    <nav>
+      <NavLink to="/">Activities</NavLink>
+
+      {token ? (
+        <button onClick={logout}>Logout</button>
+      ) : (
+        <>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/register">Register</NavLink>
+        </>
+      )}
+    </nav>
   );
 }
